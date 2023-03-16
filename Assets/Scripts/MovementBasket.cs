75e499151d92e1f12cd0a0bcc79d1aee373df529
MarceTempest <Marcelo.vidal@uniacc.edu>
2023-03-16 09:47:40 -0300

    BaseCommit
    La base

---
+++ Assets/Scripts/MovementBasket.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBasket : MonoBehaviour
+{
+    public float basketSpeed = 20;
+    public float min = -8, max = 8;
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * basketSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+
+
+
+        {
+          transform.Translate(Vector3.right * Time.deltaTime * basketSpeed);
+        }
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, min, max);
+        transform.position = pos;
+    
+
+        
+    }
+}
